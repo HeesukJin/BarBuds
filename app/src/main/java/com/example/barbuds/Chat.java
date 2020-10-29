4e5b4cf07adcef9f1f19e4bcc0034270ebc52c9f
Gafar Amuda <47370297+gafar123@users.noreply.github.com>
2020-10-28 22:55:46 -0500

    working on it

--- app/src/main/java/com/example/barbuds/Chat.java
+++ app/src/main/java/com/example/barbuds/Chat.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import firestream.chat.firestore.FirestoreService;
+import firestream.chat.namespace.Fire;
+
 public class Chat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        Fire.stream().initialize(this, new FirestoreService());
     }
 }
